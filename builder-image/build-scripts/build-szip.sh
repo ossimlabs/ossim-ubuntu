57f67232b81b67b0df1098d1ee9d5d0865a939dd
Cory Robertson <coryr@Corys-MacBook-Pro.local>
2020-05-26 09:03:27 -0400

    beginning translation of dockerfile to ubuntu

--- builder-image/build-scripts/build-szip.sh
+++ builder-image/build-scripts/build-szip.sh
@@ -3,6 +3,6 @@
 cd szip
 mkdir -p build
 cd build
-cmake3 -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j 8 install
 if [ $? -ne 0 ]; then echo "szip install error: $error" ; exit 1 ; fi
